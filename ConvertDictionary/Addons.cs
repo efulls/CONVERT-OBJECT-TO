c55814f983a527740128e6d839bc0745f346cbb7
efulls <saeful.mahmud@gmail.com>
2020-05-17 09:55:08 +0700

    Add project files.

---
+++ ConvertDictionary/Addons.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertDictionary
+{
+    public class Addons
+    {
+        public bool dns { get; set; }
+        public bool email { get; set; }
+        public bool idprotect { get; set; }
+    }
+
+    public class GracePeriod
+    {
+        public int days { get; set; }
+        public string price { get; set; }
+    }
+
+    public class Com
+    {
+        public List<string> categories { get; set; }
+        public Addons addons { get; set; }
+        public string group { get; set; }
+        public Dictionary<int, string> register { get; set; }
+        public Dictionary<int, string> transfer { get; set; }
+        public Dictionary<int, string> renew { get; set; }
+        public GracePeriod grace_period { get; set; }
+        public object redemption_period { get; set; }
+    }
+
+    public class RootObject
+    {
+        public Com com { get; set; }
+    }
+
+}
---
+++ ConvertDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = "{\"com\":{\"categories\":[\"gTLD\",\"Popular\"],\"addons\":{\"dns\":true,\"email\":true,\"idprotect\":true},\"group\":\"hot\",\"register\":{\"1\":\"11.99\",\"2\":\"23.98\",\"3\":\"35.97\",\"4\":\"47.96\",\"5\":\"59.95\",\"6\":\"71.94\",\"7\":\"83.93\",\"8\":\"95.92\",\"9\":\"107.91\",\"10\":\"119.90\"},\"transfer\":{\"1\":\"11.99\",\"2\":\"23.98\",\"3\":\"35.97\",\"4\":\"47.96\",\"5\":\"59.95\",\"6\":\"71.94\",\"7\":\"83.93\",\"8\":\"95.92\",\"9\":\"107.91\",\"10\":\"119.90\"},\"renew\":{\"1\":\"11.99\",\"2\":\"23.98\",\"3\":\"35.97\",\"4\":\"47.96\",\"5\":\"59.95\",\"6\":\"71.94\",\"7\":\"83.93\",\"8\":\"95.92\",\"9\":\"107.91\",\"10\":\"119.90\"},\"grace_period\":{\"days\":30,\"price\":\"\u00a30.00\"},\"redemption_period\":null}}";
+
+            var result = JsonConvert.DeserializeObject<RootObject>(json);
+
+            Console.WriteLine(result);
+            Console.WriteLine(json);
+        }
+    }
+}
